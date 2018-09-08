238b9c2f4d8c82a7780483579940589601cd8c89
redrees <svhk109@gmail.com>
2018-09-08 00:59:35 -0400

    starting COMP6600 HW1

--- TestJava/src/mypackage/MyMain.java
+++ TestJava/src/mypackage/MyMain.java
@@ -6,6 +6,22 @@ public class MyMain {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
 		System.out.println("Another line!");
+		
+		int[] initial = {
+				2, 3, 7, 4, 5,
+				1, -1, 11, -1, 8,
+				6, 10, 0, 12, 15,
+				9, -1, 14, -1, 20,
+				13, 16, 17, 18, 19
+		};
+		
+		int[] goal = {
+				1, 2, 3, 4, 5,
+				6, -1, 7, -1, 8,
+				9, 10, 0, 11, 12,
+				13, -1, 14, -1, 15,
+				16, 17, 18, 19, 20
+		};
 	}
 
 }
